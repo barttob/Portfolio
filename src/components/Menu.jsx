7b40ee2e32507ac2c31f1068fd2644eac1e5e78c
barttob <barttob@gmail.com>
2023-02-12 15:23:04 +0100

    Responsive base

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,61 @@
 import React from "react";
 
 import Scroll from "../subComponents/Scroll";
-import ThemeToggle from "../subComponents/ThemeToggle";
 
 import "../styles/Menu.css";
+
+import "../styles/ThemeToggle.css";
+
+const setDark = () => {
+  localStorage.setItem("theme", "dark");
+  document.documentElement.setAttribute("data-theme", "dark");
+};
+
+const setLight = () => {
+  localStorage.setItem("theme", "light");
+  document.documentElement.setAttribute("data-theme", "light");
+};
+
+const storedTheme = localStorage.getItem("theme");
+
+const prefersDark =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const defaultDark =
+  storedTheme === "dark" || (storedTheme === null && prefersDark);
+
+if (defaultDark) {
+  setDark();
+}
+
+const toggleTheme = (e) => {
+  if (e.target.checked) {
+    setDark();
+  } else {
+    setLight();
+  }
+};
 
 const Menu = ({ scrollPosition }) => {
   return (
     <div className="menu">
       <a href="#">
-        <img src="HEXLogoLight.svg" alt="logo" color="#fff"/>
+        {/* <img src="HEXLogoLight.svg" alt="logo" className="menu__logo"/> */}
+        <svg
+
+          viewBox="0 0 83 64"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="menu__logo"
+        >
+          <g clip-path="url(#clip0_19_4)">
+            <path
+              d="M6.33052 6.4L19.1538 0L44.6923 12.8L70.2308 0L83 6.4L63.8462 16V60.8L57.4615 64L51.0769 60.8V22.4L31.9231 32L44.6923 38.4V51.2L19.1538 64L0 54.4V41.6L19.1538 51.2L31.9231 44.8L0 28.8V16L19.1538 25.6L31.9231 19.2L6.33052 6.4Z"
+              fill="#121912"
+            />
+          </g>
+        </svg>
       </a>
       <div className="menu__list--under">
         <div className="menu__list">
@@ -18,7 +64,8 @@ const Menu = ({ scrollPosition }) => {
             href="#"
             style={
               scrollPosition < 26
-                ? { fontWeight: "bold", textDecoration: "underline" }
+                ? { fontWeight: "bold" }
+                // ? { fontWeight: "bold", textDecoration: "underline" }
                 : { fontWeight: "normal" }
             }
           >
@@ -29,7 +76,7 @@ const Menu = ({ scrollPosition }) => {
             href="#about"
             style={
               scrollPosition >= 26 && scrollPosition < 58
-                ? { fontWeight: "bold", textDecoration: "underline" }
+                ? { fontWeight: "bold" }
                 : { fontWeight: "normal" }
             }
           >
@@ -40,7 +87,7 @@ const Menu = ({ scrollPosition }) => {
             href="#work"
             style={
               scrollPosition >= 58 && scrollPosition < 82
-                ? { fontWeight: "bold", textDecoration: "underline" }
+                ? { fontWeight: "bold" }
                 : { fontWeight: "normal" }
             }
           >
@@ -51,7 +98,7 @@ const Menu = ({ scrollPosition }) => {
             href="#contact"
             style={
               scrollPosition >= 82
-                ? { fontWeight: "bold", textDecoration: "underline" }
+                ? { fontWeight: "bold" }
                 : { fontWeight: "normal" }
             }
           >
@@ -60,7 +107,21 @@ const Menu = ({ scrollPosition }) => {
         </div>
         <Scroll />
       </div>
-      <ThemeToggle />
+      <div className="toggle-theme-wrapper">
+        {/* <span>☀️</span> */}
+        <img src="SVG/Sun.svg" alt="moon" />
+        <label className="toggle-theme" htmlFor="checkbox">
+          <input
+            type="checkbox"
+            id="checkbox"
+            onChange={toggleTheme}
+            defaultChecked={defaultDark}
+          />
+          <div className="toggler round"></div>
+        </label>
+        {/* <span>🌒</span> */}
+        <img src="SVG/Moon.svg" alt="moon" />
+      </div>
     </div>
   );
 };
